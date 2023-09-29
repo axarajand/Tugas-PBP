a17b1a2311b04e149577ee1a942582fd7e5c433e
axarajand <axarajand@gmail.com>
2023-09-29 20:31:12 +0700

    #2 Tipe Data

---
+++ #2 Tipe Data/tipe_data.js
@@ -0,0 +1,8 @@
+let firstName = "Axa"; 
+let lastName = "Rajandrya"; 
+let age = 20; 
+let isMarried = false; 
+
+console.log("Nama Lengkap: " + firstName + " " + lastName);
+console.log("Umur: " + age);
+console.log("Status Pernikahan: " + (isMarried ? "Menikah" : "Belum Menikah"));
